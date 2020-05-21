3dcb946574eda8e1b2022d41480bad0ac8b38c54
Rizki Afandi <64829987+rafandi69@users.noreply.github.com>
2020-05-21 22:17:00 +0700

    buat tampilan atas. belum fix
    kalau bisa dikonneksikan

---
+++ kotak.php
@@ -0,0 +1,66 @@
+<div class="col-md-12">
+<section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-blue">
+              <div class="inner">
+             <h3>0</h3>
+
+                <p>Data Laboratorium</p>
+              </div>
+              <div class="icon">
+              <ion-icon name="desktop-sharp"></ion-icon>
+              </div>
+              <a <?php ('master/tipe_laboratorium/v_tipe_laboratorium_list.php')?> class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+              <div class="inner">
+                <h3>0</sup></h3>
+
+                <p>Data Alat</p>
+              </div>
+              <div class="icon">
+              <ion-icon name="construct-sharp"></ion-icon>
+              </div>
+              <a <?php ('master/nama_alat/v_nama_alat_list.php')?> class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>Data Bahan</p>
+              </div>
+              <div class="icon">
+              <ion-icon name="flask-sharp"></ion-icon>
+              </div>
+              <a <?php  ('master/nama_bahan/v_master_bahan_list.php')?> class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-red">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>Data Modul</p>
+              </div>
+              <div class="icon">
+              <ion-icon name="journal-sharp"></ion-icon>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
