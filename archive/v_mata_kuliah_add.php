fc13a3990731cb7b70f03797f9b038186559871d
Rizki Afandi <sunankarebet@gmail.com>
2020-05-31 20:21:32 +0700

    ARCHIVE

---
+++ archive/v_mata_kuliah_add.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?php
+    $row = fetch_single_row($edit);
+?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Kode</label>
+            <div class="col-sm-8">
+            <?php echo form_hidden('id',$row->id); ?>
+            <?php echo form_input(array('name'=>'kode','value'=>$row->kode,'class'=>'form-control'));?>
+            <?php echo form_error('kode');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Mata Kuliah</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'mata_kuliah','value'=>$row->mata_kuliah,'class'=>'form-control'));?>
+            <?php echo form_error('mata_kuliah');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Jumlah SKS</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'sks','value'=>$row->sks,'class'=>'form-control'));?>
+            <?php echo form_error('sks');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Simpan</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/mata_kuliah/show_editForm/","#divsubcontent")','Simpan','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $(".select2").select2();
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
