a8596b03326f2d509744f1e16df460bf47f78e5a
aldabernikaifada <64902871+aldabernikaifada@users.noreply.github.com>
2020-06-08 21:22:22 +0700

    update kelola laboratorium

---
+++ application/views/kelola/laboratorium/v_laboratorium_add.php
@@ -0,0 +1,62 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Nama Lab</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'nama_lab','class'=>'form-control'));?>
+            <?php echo form_error('nama_lab');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Alamat Lab</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'alamat_lab','class'=>'form-control'));?>
+            <?php echo form_error('alamat_lab');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Korlab</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'korlab','class'=>'form-control'));?>
+            <?php echo form_error('korlab');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Laboran</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'laboran','class'=>'form-control'));?>
+            <?php echo form_error('laboran');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Status</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'Status','class'=>'form-control'));?>
+            <?php echo form_error('Status');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Simpan</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"kelola/laboratorium/show_addForm/","#divsubcontent")','simpan','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+    $(".select2").select2();
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+</script>
