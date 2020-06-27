cabe02f8a939086ed33710a247411805a727d48a
Rizki Afandi <r.afandi.21@students.unnes.ac.id>
2020-06-27 09:14:12 +0700

    berita

--- application/views/cms/berita/v_berita_add.php
+++ application/views/cms/berita/v_berita_add.php
@@ -1,13 +1,27 @@
-
-    <div>
-        <form>
-            <textarea name="editor1" id="editor1" rows="10" cols="80">
-                This is my textarea to be replaced with CKEditor 4.
-            </textarea>
+<div class="box box-info">
+            <div class="box-header">
+              <h3 class="box-title">Isian Berita
+              </h3>
+              <!-- tools box -->
+              <div class="pull-right box-tools">
+                <button type="button" class="btn btn-info btn-sm" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
+                  <i class="fa fa-minus"></i></button>
+                <button type="button" class="btn btn-info btn-sm" data-widget="remove" data-toggle="tooltip" title="" data-original-title="Remove">
+                  <i class="fa fa-times"></i></button>
+              </div>
+              <!-- /. tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body pad">
+              <form>
+                    <textarea id="editor1" name="editor1" rows="10" cols="80" style="visibility: hidden; display: none;">                                            This is my textarea to be replaced with CKEditor.
+                    </textarea>
+   
             <script>
                 // Replace the <textarea id="editor1"> with a CKEditor 4
                 // instance, using default configuration.
                 CKEDITOR.replace( 'editor1' );
             </script>
         </form>
+</div>
 </div>
--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -1,9 +1,9 @@
 <div class="row">
-    <div class="col-lg-12">
-    <div class="form-group">
+    <div class="col-md-12">
+    <div class="form-group col-md-12">
     <?php require_once ('application/views/kotak/kotak.php') ?>
             </div>
-            <div class="form-group">
+            <div class="form-group col-sm-6">
                 <?php require_once ('application/views/cms/berita/v_berita_add.php') ?>
             </div>
     </div>
