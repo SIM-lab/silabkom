756751fa68864527267020b8e3e8d2b1d05b6474
Dwika Ananda Agustina Pertiwi <64952312+dwikagt@users.noreply.github.com>
2020-06-28 22:09:55 +0700

    Update v_satuan_list.php

--- application/views/master/satuan/v_satuan_list.php
+++ application/views/master/satuan/v_satuan_list.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-<?php require_once ('application/views/kotak.php');?>
-=======
-<?php require_once ('application/views/kotak/kotak.php') ?>
->>>>>>> 6c053859d02217527dbf41f050fffd44456b3823
+<?php require_once ('application/views/kotak/kotak.php');?>
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
 
     <div class="row" id="form_pembelian">
