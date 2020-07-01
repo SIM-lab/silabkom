a537a18e4ec434e83107c961af716f1d0293c8f2
Rizki Afandi <r.afandi.21@students.unnes.ac.id>
2020-07-01 14:25:10 +0700

    allowed level jajal cek meneh

--- database potongan/cms_menu.sql
+++ database potongan/cms_menu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2020 at 04:49 PM
+-- Generation Time: Jul 01, 2020 at 09:23 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -48,9 +48,9 @@ INSERT INTO `cms_menu` (`id`, `alias`, `nama`, `deskripsi`, `allowed_level`, `ur
 (3, 'menu', 'Menu', 'Untuk memanajemen menu administrasi (back-end)', '+1+', 'cms/manage/menu', 1, 7, 1),
 (89, 'reset_pass', 'Reset Password User', 'Reset Password User', '+1+', 'apl/reset', 1, 1, 3),
 (35, 'semua', 'semua', 'untuk login semua', '+1+2+3+4+5+6+8+7+', 'cms/login/admin_page', 1, 1, 3),
-(88, 'tambah_user', 'Tambah User', '', '+1+2+3+4+', 'cms/manage/form_add_user', 1, 1, 3),
-(102, 'log', 'Log System', 'Log System', '+1+2+', 'cms/catatan/catat', 1, 2, 1),
-(136, 'satuan', 'Satuan', '-', '+1+2+', 'master/satuan', 1, 9, 2),
+(88, 'tambah_user', 'Tambah User', '', '+1+', 'cms/manage/form_add_user', 1, 1, 3),
+(102, 'log', 'Log System', 'Log System', '+1+2+3+4+5+', 'cms/catatan/catat', 1, 2, 1),
+(136, 'satuan', 'Satuan', '-', '+1+3+', 'master/satuan', 1, 9, 2),
 (101, 'status_pak', 'Home', 'Status Pengajuan PAK', '+1+2+3+4+5+6+', 'pak/dashboard/status_pak', 1, 1, 101),
 (135, 'master_bahan', 'Nama Bahan', '-', '+1+2+', 'master/master_bahan', 1, 8, 2),
 (134, 'nama_alat', 'Nama Alat', '-', '+1+2+', 'master/nama_alat', 1, 7, 2),
@@ -60,14 +60,18 @@ INSERT INTO `cms_menu` (`id`, `alias`, `nama`, `deskripsi`, `allowed_level`, `ur
 (130, 'kategori_no_induk', 'Kategori Nomer Induk', '-', '+1+2+', 'master/kategori_no_induk', 1, 3, 2),
 (129, 'gambar_depan', 'Gambar Depan', '-', '+1+2+', 'master/gambar_depan', 1, 2, 2),
 (128, 'instansi', 'Instansi', '-', '+1+2+', 'master/instansi', 1, 1, 2),
-(173, 'periode_pengajuan', 'Pengajuan', '', '+1+2+3+4+5+6+8+7+', 'pengajuan/periode_pengajuan', 1, 1, 4),
+(173, 'periode_pengajuan', 'Pengajuan', '', '+1+2+3+4+5+6+', 'pengajuan/periode_pengajuan', 1, 1, 4),
 (174, 'pengajuan_bahan', 'Pengajuan Bahan', 'Pengajuan Bahan', '+1+2+3+4+5+6+', 'pengajuan/pengajuan_bahan', 1, 2, 4),
-(175, 'pengajuan_alat', 'Pengajuan Alat', 'Pengajuan Alat', '+1+2+3+', 'pengajuan/pengajuan_alat', 1, 3, 4),
-(176, 'jatuh_tempo', 'Jatuh Tempo', '', '+1+2+3+4+5+6+8+7+', 'peminjaman/jatuh_tempo', 1, 1, 5),
-(177, 'user', 'User', '', '+1+2+3+4+5+6+8+7', 'cms/user', 1, 1, 1),
-(178, 'modul', 'Modul', 'modal modul', '+1+2+3+4+5+6+7+8', 'master/modul', 1, 10, 2),
+(175, 'pengajuan_alat', 'Pengajuan Alat', 'Pengajuan Alat', '+1+2+3+4+5+6', 'pengajuan/pengajuan_alat', 1, 3, 4),
+(176, 'jatuh_tempo', 'Jatuh Tempo', '', '+1+2+3+4+5+6+7+', 'peminjaman/jatuh_tempo', 1, 1, 5),
+(177, 'user', 'User', '', '+1+', 'cms/user', 1, 1, 6),
+(178, 'modul', 'Modul', 'modal modul', '+1+2+3+4+5+6+7+', 'master/modul', 1, 10, 2),
 (179, 'kelola_penggantian', 'Penggantian', '', '+1+2+3+', 'kelola/kelola_penggantian', 1, 3, 6),
-(180, 'peminjaman_alat', 'Alat', '', '+1+2+3+4+5=6+7+8', 'peminjaman/peminjaman_alat', 1, 2, 5);
+(180, 'peminjaman_alat', 'Alat', '', '+1+2+3+4+5+6+7+8', 'peminjaman/peminjaman_alat', 1, 2, 5),
+(181, 'laboratorium', 'Laboratorium', '', '+1+2+3+4+5+6+7+', 'kelola/laboratorium', 1, 2, 6),
+(182, 'cek_status', 'Cek Status', '', '+1+2+3+4+5+6+7+8+', 'peminjaman/cek_status', 1, 3, 5),
+(183, 'jadwal', 'Jadwal', '', '+1+2+3+4+5+6+7+8', 'kelola/jadwal', 1, 5, 6),
+(184, 'kelola_alat', 'Kelola Alat', '', '+1+2+3+4+5+6+7+8+', 'kelola/kelola_alat', 1, 4, 6);
 
 --
 -- Indexes for dumped tables
@@ -87,7 +91,7 @@ ALTER TABLE `cms_menu`
 -- AUTO_INCREMENT for table `cms_menu`
 --
 ALTER TABLE `cms_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=185;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
